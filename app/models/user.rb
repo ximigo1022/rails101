b9f027afbadf47922e5c0c705fc6fbc04acbb125
ximigo1022 <liaohuatang1208@gmail.com>
2016-12-22 18:20:39 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,13 @@ class User < ApplicationRecord
        end
 
 
+       def join!(group)
+           participated_groups << group
+         end
+
+         def quit!(group)
+           participated_groups.delete(group)
+         end
+
+
 end
